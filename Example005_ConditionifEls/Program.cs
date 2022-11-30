46b319d763fe6a84edb0cd176e931a8e41d31b8e
Poopa <poopa@loopa.za>
2022-11-30 10:46:48 +0300

    Example005 Condition if-Els

---
+++ Example005_ConditionifEls/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower()== "морти")
+{
+    Console.WriteLine("Опять ты, Морти!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
